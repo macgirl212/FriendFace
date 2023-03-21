307dc9e1df5d1057ada362d627481310702a526a
Melody Davis <piano.fan122@gmail.com>
2023-03-21 14:50:18 -0400

    Initial Commit

---
+++ FriendFace/FriendFaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendFaceApp.swift
+//  FriendFace
+//
+//  Created by Melody Davis on 3/21/23.
+//
+
+import SwiftUI
+
+@main
+struct FriendFaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
